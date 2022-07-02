94a1b09537b48077fabd8cc60ffaca3dcf09486c
kaememerel <kaememerel@users.noreply.github.com>
2022-07-02 12:23:14 +0200

    Update README 1.1

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 Funkcjonalnosci aplikacji v1.0:
 - [x] obsluga osob doroslych
 - [x] obsluga wymagan dla osob +40
+
+v.1.1
+- [x] zwiekszenie limitu do 50 lat bez ostrzezenia
+- [ ] lepsza obsługa osób osób j/w
--- tests.py
+++ tests.py
@@ -3,9 +3,9 @@ wiek = input("Podaj wiek uzytkownika: ")
 if wiek.isdigit() == False:
     exit("Wiek musi byc podany jako liczba")
 wiek = int(wiek)
-if wiek >= 18 and wiek <= 40:
+if wiek >= 18 and wiek <= 50:
     print("Witaj w naszym sklepie z alkoholem")
-elif wiek > 40:
+elif wiek > 50:
     print("W Twoim wieku alkohol jest juz szkodliwy")
 else:
     exit("Jestes za mlody na alkohol!")
